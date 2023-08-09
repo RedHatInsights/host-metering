0cc9d3292cc32234eab9115120b29f4e395f4391
Petr Vobornik <pvoborni@redhat.com>
2023-08-09 18:00:41 +0200

    feat(config): loading from environmetal variables
    Where order of preference is:
    
    CLI > EnvVar > Default
    
    Signed-off-by: Petr Vobornik <pvoborni@redhat.com>

--- config/config.go
+++ config/config.go
@@ -1,6 +1,10 @@
 package config
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
 
 const (
 	DefaultConfigPath           = "/etc/milton.conf"
@@ -25,12 +29,12 @@ type Config struct {
 	WriteRetryInterval   uint // in seconds
 }
 
-func NewConfig(writeUrl string, writeInterval uint, certPath string, keyPath string) *Config {
+func NewConfig() *Config {
 	return &Config{
-		WriteUrl:             writeUrl,
-		WriteInterval:        writeInterval,
-		HostCertPath:         certPath,
-		HostCertKeyPath:      keyPath,
+		WriteUrl:             DefaultWriteUrl,
+		WriteInterval:        DefaultWriteInterval,
+		HostCertPath:         DefaultCertPath,
+		HostCertKeyPath:      DefaultKeyPath,
 		CollectInterval:      DefaultCollectInterval,
 		LabelRefreshInterval: DefaultLabelRefreshInterval,
 		WriteRetryAttempts:   DefaultWriteRetryAttempts,
@@ -48,4 +52,49 @@ func (c *Config) Print() {
 	fmt.Println("  LabelRefreshInterval: ", c.LabelRefreshInterval)
 	fmt.Println("  WriteRetryAttempts: ", c.WriteRetryAttempts)
 	fmt.Println("  WriteRetryInterval: ", c.WriteRetryInterval)
+}
+
+func (c *Config) UpdateFromCliOptions(writeUrl string, writeInterval uint, certPath string, keyPath string) {
+	if writeUrl != "" {
+		c.WriteUrl = writeUrl
+	}
+	if writeInterval != 0 {
+		c.WriteInterval = writeInterval
+	}
+	if certPath != "" {
+		c.HostCertPath = certPath
+	}
+	if keyPath != "" {
+		c.HostCertKeyPath = keyPath
+	}
+}
+
+func parseEnvVarUint(name string, currentValue uint) uint {
+	if v := os.Getenv(name); v != "" {
+		val, err := strconv.ParseUint(v, 10, 32)
+		if err == nil {
+			return uint(val)
+		} else {
+			fmt.Printf("Error parsing var: %s %v %s\n", name, v, err)
+		}
+	}
+	return currentValue
+}
+
+func (c *Config) UpdateFromEnvVars() {
+	fmt.Println("Updating config from environment variables...")
+	if v := os.Getenv("MILTON_WRITE_URL"); v != "" {
+		c.WriteUrl = v
+	}
+	c.WriteInterval = parseEnvVarUint("MILTON_WRITE_INTERVAL", c.WriteInterval)
+	if v := os.Getenv("MILTON_HOST_CERT"); v != "" {
+		c.HostCertPath = v
+	}
+	if v := os.Getenv("MILTON_HOST_KEY"); v != "" {
+		c.HostCertKeyPath = v
+	}
+	c.CollectInterval = parseEnvVarUint("MILTON_COLLECT_INTERVAL", c.CollectInterval)
+	c.LabelRefreshInterval = parseEnvVarUint("MILTON_LABEL_REFRESH_INTERVAL", c.LabelRefreshInterval)
+	c.WriteRetryAttempts = parseEnvVarUint("MILTON_WRITE_RETRY_ATTEMPTS", c.WriteRetryAttempts)
+	c.WriteRetryInterval = parseEnvVarUint("MILTON_WRITE_RETRY_INTERVAL", c.WriteRetryInterval)
 }
--- main.go
+++ main.go
@@ -10,10 +10,10 @@ import (
 )
 
 func main() {
-	writeUrl := flag.String("write-url", config.DefaultWriteUrl, "Prometheus remote write endpoint")
-	tick := flag.Uint("tick", config.DefaultWriteInterval, "Report every tick seconds")
-	certPath := flag.String("cert", config.DefaultCertPath, "Host certificate path")
-	keyPath := flag.String("key", config.DefaultKeyPath, "Host certificate key path")
+	writeUrl := flag.String("write-url", "", "Prometheus remote write endpoint")
+	tick := flag.Uint("tick", 0, "Report every tick seconds")
+	certPath := flag.String("cert", "", "Host certificate path")
+	keyPath := flag.String("key", "", "Host certificate key path")
 
 	flag.NewFlagSet("help", flag.ExitOnError)
 	flag.NewFlagSet("daemon", flag.ExitOnError)
@@ -33,7 +33,9 @@ func main() {
 		printUsage()
 		return
 	case "daemon", "once":
-		cfg := config.NewConfig(*writeUrl, *tick, *certPath, *keyPath)
+		cfg := config.NewConfig()
+		cfg.UpdateFromEnvVars()
+		cfg.UpdateFromCliOptions(*writeUrl, *tick, *certPath, *keyPath)
 		cfg.Print()
 		hostInfo, err := hostinfo.LoadHostInfo(cfg)
 		if err != nil {

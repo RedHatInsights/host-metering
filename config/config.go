9c4e368984c2e781e71a44abc5162b2df268e450
Petr Vobornik <pvoborni@redhat.com>
2023-08-09 17:04:01 +0200

    config: add additional configuration option
    Extending only the config structure, loading and usage will be added
    later.
    
    Signed-off-by: Petr Vobornik <pvoborni@redhat.com>

--- config/config.go
+++ config/config.go
@@ -3,18 +3,26 @@ package config
 import "fmt"
 
 type Config struct {
-	WriteUrl        string
-	WriteInterval   uint // in seconds
-	HostCertPath    string
-	HostCertKeyPath string
+	WriteUrl             string
+	WriteInterval        uint // in seconds
+	CollectInterval      uint // in seconds
+	LabelRefreshInterval uint // in seconds
+	HostCertPath         string
+	HostCertKeyPath      string
+	WriteRetryAttempts   uint
+	WriteRetryInterval   uint // in seconds
 }
 
 func NewConfig(writeUrl string, writeInterval uint, certPath string, keyPath string) *Config {
 	return &Config{
-		WriteUrl:        writeUrl,
-		WriteInterval:   writeInterval,
-		HostCertPath:    certPath,
-		HostCertKeyPath: keyPath,
+		WriteUrl:             writeUrl,
+		WriteInterval:        writeInterval,
+		HostCertPath:         certPath,
+		HostCertKeyPath:      keyPath,
+		CollectInterval:      0,
+		LabelRefreshInterval: 86400,
+		WriteRetryAttempts:   3,
+		WriteRetryInterval:   1,
 	}
 }
 
@@ -24,4 +32,8 @@ func (c *Config) Print() {
 	fmt.Println("  WriteInterval: ", c.WriteInterval)
 	fmt.Println("  HostCertPath: ", c.HostCertPath)
 	fmt.Println("  HostCertKeyPath: ", c.HostCertKeyPath)
+	fmt.Println("  CollectInterval: ", c.CollectInterval)
+	fmt.Println("  LabelRefreshInterval: ", c.LabelRefreshInterval)
+	fmt.Println("  WriteRetryAttempts: ", c.WriteRetryAttempts)
+	fmt.Println("  WriteRetryInterval: ", c.WriteRetryInterval)
 }

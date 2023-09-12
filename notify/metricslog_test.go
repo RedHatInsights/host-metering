c3446ec158be7251393c6b31b37c49fea6379046
Vendula Poncova <vponcova@redhat.com>
2023-09-12 16:56:09 +0200

    test: Simplify the metrics log tests
    Introduce new functions to make the tests easier to follow and modify.

--- notify/metricslog_test.go
+++ notify/metricslog_test.go
@@ -1,114 +1,66 @@
 package notify
 
 import (
+	"github.com/prometheus/prometheus/prompb"
 	"testing"
 )
 
 // Test basic functionality of the MetricsLog - how it would be used by milton
 func TestMetricsLogBasics(t *testing.T) {
 	// Create a new MetricsLog instance
-	dir := t.TempDir()
-	logPath := dir + "/metrics"
-	log, err := NewMetricsLog(logPath)
-	if err != nil {
-		t.Fatalf("failed to create MetricsLog: %v", err)
-	}
+	log, err := NewMetricsLog(createMetricsPath(t))
+	checkError(t, err, "failed to create MetricsLog")
 	defer log.Close()
 
 	// Write some sample cpuCount data to the log
 	err = log.Write(4)
-	if err != nil {
-		t.Fatalf("failed to write sample data to MetricsLog: %v", err)
-	}
+	checkError(t, err, "failed to write sample data to MetricsLog")
 
 	// Another sample
 	err = log.Write(6)
-	if err != nil {
-		t.Fatalf("failed to write sample data to MetricsLog: %v", err)
-	}
+	checkError(t, err, "failed to write sample data to MetricsLog")
 
 	// Get all samples from the log
 	samples, lastIndex, err := log.GetAllSamples()
-	if err != nil {
-		t.Fatalf("failed to get samples from MetricsLog: %v", err)
-	}
-
-	// Verify that the last index is 2 as there should be 2 samples
-	if lastIndex != 2 {
-		t.Fatalf("expected last index of 2, got %d", lastIndex)
-	}
-
-	// Verify that the sample data was written correctly
-	if len(samples) != 2 {
-		t.Fatalf("expected 2 sample, got %d", len(samples))
-	}
-	if samples[0].Value != 4 {
-		t.Fatalf("expected sample value of 4, got %f", samples[0].Value)
-	}
-	if samples[1].Value != 6 {
-		t.Fatalf("expected sample value of 6, got %f", samples[1].Value)
-	}
+	checkError(t, err, "failed to get samples from MetricsLog")
+	checkSamples(t, samples, 4, 6)
+	checkIndex(t, lastIndex, 2)
 
 	// Truncate the log to the last item read
 	err = log.TruncateTo(lastIndex)
-	if err != nil {
-		t.Fatalf("failed to truncate MetricsLog: %v", err)
-	}
+	checkError(t, err, "failed to truncate MetricsLog")
 
 	// Get all samples from the log again, to check that the truncation worked
 	samples, lastIndex, err = log.GetAllSamples()
-	if err != nil {
-		t.Fatalf("failed to get samples from MetricsLog: %v", err)
-	}
-
-	// Verify that the log is now empty
-	if len(samples) != 0 {
-		t.Fatalf("expected 0 samples, got %d", len(samples))
-	}
-	// Last index should still the same
-	if lastIndex != 2 {
-		t.Fatalf("expected last index of 2, got %d", lastIndex)
-	}
+	checkError(t, err, "failed to get samples from MetricsLog")
+	checkSamples(t, samples)
+	checkIndex(t, lastIndex, 2)
 
 	// Try again, but this time there is nothing to truncate
 	err = log.TruncateTo(lastIndex)
-	if err != nil {
-		t.Fatalf("failed to truncate MetricsLog when there is nothing to truncate: %v", err)
-	}
+	checkError(t, err, "failed to truncate MetricsLog")
 
 	// Simulate next iteration: write sample, obtain it and truncate
 	err = log.Write(8)
-	if err != nil {
-		t.Fatalf("failed to write sample data to MetricsLog: %v", err)
-	}
-	samples, lastIndex, err = log.GetAllSamples()
-	if err != nil {
-		t.Fatalf("failed to get samples from MetricsLog: %v", err)
-	}
-	if len(samples) != 1 {
-		t.Fatalf("expected 1 sample, got %d", len(samples))
-	}
-	if samples[0].Value != 8 {
-		t.Fatalf("expected sample value of 8, got %f", samples[0].Value)
-	}
-	err = log.TruncateTo(lastIndex)
-	if err != nil {
-		t.Fatalf("failed to truncate MetricsLog: %v", err)
-	}
+	checkError(t, err, "failed to write sample data to MetricsLog")
 
+	samples, lastIndex, err = log.GetAllSamples()
+	checkError(t, err, "failed to get samples from MetricsLog")
+	checkSamples(t, samples, 8)
+	checkIndex(t, lastIndex, 3)
+
+	err = log.TruncateTo(lastIndex)
+	checkError(t, err, "failed to truncate MetricsLog")
 }
 
 // Test scenario where Prometheus server is not initially reachable
 // (log is not truncated). And milton is restarted in the meantime.
 func TestRestart(t *testing.T) {
-	dir := t.TempDir()
-	logPath := dir + "/metrics"
+	logPath := createMetricsPath(t)
 
 	// First run of milton
 	log, err := NewMetricsLog(logPath)
-	if err != nil {
-		t.Fatalf("failed to create MetricsLog: %v", err)
-	}
+	checkError(t, err, "failed to create MetricsLog")
 
 	// Write some sample cpuCount data to the log
 	log.Write(1) // index 1
@@ -117,15 +69,13 @@ func TestRestart(t *testing.T) {
 	log.Write(4) // index 4
 	log.Write(5) // index 5
 
-	samples, lastIndex, _ := log.GetAllSamples()
-	if len(samples) != 5 {
-		t.Fatalf("expected 5 samples, got %d", len(samples))
-	}
-	if lastIndex != 5 {
-		t.Fatalf("expected last index of 5, got %d", lastIndex)
-	}
+	samples, lastIndex, err := log.GetAllSamples()
+	checkError(t, err, "failed to get samples from MetricsLog")
+	checkSamples(t, samples, 1, 2, 3, 4, 5)
+	checkIndex(t, lastIndex, 5)
 
-	log.Close()
+	err = log.Close()
+	checkError(t, err, "failed to close MetricsLog")
 
 	// Second run of milton
 	log, _ = NewMetricsLog(logPath)
@@ -136,30 +86,63 @@ func TestRestart(t *testing.T) {
 	// It could be solved by recording the last index in a separate file. But
 	// that migth be unnecessary added complexity and this behavior could be
 	// acceptable.
-	samples, lastIndex, _ = log.GetAllSamples()
-	if len(samples) != 4 {
-		t.Fatalf("expected 4 samples, got %d", len(samples))
-	}
-	if samples[0].Value != 2 {
-		t.Fatalf("expected first sample to have value of 2, got %f", samples[0].Value)
-	}
-	if lastIndex != 5 {
-		t.Fatalf("expected last index of 5, got %d", lastIndex)
-	}
+	samples, lastIndex, err = log.GetAllSamples()
+	checkError(t, err, "failed to get samples from MetricsLog")
+	checkSamples(t, samples, 2, 3, 4, 5)
+	checkIndex(t, lastIndex, 5)
 
-	log.TruncateTo(lastIndex)
-	log.Close()
+	err = log.TruncateTo(lastIndex)
+	checkError(t, err, "failed to truncate MetricsLog")
+
+	err = log.Close()
+	checkError(t, err, "failed to close MetricsLog")
 
 	// Third run of milton - after log was truncated
-	log, _ = NewMetricsLog(logPath)
+	log, err = NewMetricsLog(logPath)
+	checkError(t, err, "failed to create MetricsLog")
 	defer log.Close()
 
-	samples, lastIndex, _ = log.GetAllSamples()
-	if len(samples) != 0 {
-		t.Fatalf("expected that after truncation the samples to send is 0, got %d", len(samples))
+	samples, lastIndex, err = log.GetAllSamples()
+	checkError(t, err, "failed to get samples from MetricsLog")
+	checkSamples(t, samples)
+	checkIndex(t, lastIndex, 5)
+}
+
+func createMetricsPath(t *testing.T) string {
+	dir := t.TempDir()
+	return dir + "/metrics"
+}
+
+func checkError(t *testing.T, err error, message string) {
+	if err != nil {
+		t.Fatalf("%s: %v", message, err)
+	}
+}
+
+func checkIndex(t *testing.T, index uint64, expected uint64) {
+	if index != expected {
+		t.Fatalf("unexpected index: %d != %d", index, expected)
+	}
+}
+
+func checkSamples(t *testing.T, samples []prompb.Sample, expected ...float64) {
+	// Check the expected number of samples.
+	if len(samples) != len(expected) {
+		t.Fatalf("unexpected number of samples: %d != %d", len(samples), len(expected))
 	}
 
-	if lastIndex != 5 {
-		t.Fatalf("expected last index of 5, got %d", lastIndex)
+	// Check the expected values of samples.
+	for i := 0; i < len(samples); i++ {
+		if samples[i].Value != expected[i] {
+			t.Fatalf("unexpected value of the sample %d: %f != %f", i, samples[i].Value, expected[i])
+		}
+	}
+
+	// Check timestamps of the samples.
+	for i := 1; i < len(samples); i++ {
+		if samples[i].Timestamp < samples[i-1].Timestamp {
+			t.Fatalf("unexpected timestamp of the sample %d: %d < %d",
+				i, samples[i].Timestamp, samples[i-1].Timestamp)
+		}
 	}
 }

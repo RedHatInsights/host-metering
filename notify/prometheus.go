eb49b5e43e5db1683c30693640dbf51e3dcf6637
Petr Vobornik <pvoborni@redhat.com>
2023-09-20 12:06:52 +0000

    feat: reuse http client for notification
    - moves code from mtls.go to prometheus.go and makes it internal so
      that PrometheusNotifier have full control on creation of http client
    - client is created only when HostID changes (assuming that the change
      changes certs and thus http client recreation reads the new certs)
    - makes function which should not be used by others internal
    
    Signed-off-by: Petr Vobornik <pvoborni@redhat.com>

--- notify/prometheus.go
+++ notify/prometheus.go
@@ -2,6 +2,7 @@ package notify
 
 import (
 	"bytes"
+	"crypto/tls"
 	"fmt"
 	"io"
 	"net/http"
@@ -17,8 +18,15 @@ import (
 
 // See https://prometheus.io/docs/concepts/remote_write_spec/ for specifiction of Prometheus remote write
 
+const requestTimeout time.Duration = 60 * time.Second
+
+// Should be used only for testing
+var tlsInsecureSkipVerify = false
+
 type PrometheusNotifier struct {
-	cfg *config.Config
+	cfg    *config.Config
+	lastId string
+	client *http.Client
 }
 
 func NewPrometheusNotifier(cfg *config.Config) *PrometheusNotifier {
@@ -28,25 +36,49 @@ func NewPrometheusNotifier(cfg *config.Config) *PrometheusNotifier {
 }
 
 func (n *PrometheusNotifier) Notify(samples []prompb.Sample, hostinfo *hostinfo.HostInfo) error {
-	return PrometheusRemoteWrite(hostinfo, n.cfg, samples)
+	if n.lastId != hostinfo.HostId {
+		if err := n.createHttpClient(); err != nil {
+			return err
+		}
+		n.lastId = hostinfo.HostId
+	}
+	request, err := newPrometheusRequest(hostinfo, n.cfg, samples)
+	if err != nil {
+		return err
+	}
+	return prometheusRemoteWrite(n.client, n.cfg, request)
 }
 
-func PrometheusRemoteWrite(hostinfo *hostinfo.HostInfo, cfg *config.Config, samples []prompb.Sample) error {
-	req, err := NewPrometheusRequest(hostinfo, cfg, samples)
+func (n *PrometheusNotifier) createHttpClient() error {
+	keypair, err := tls.LoadX509KeyPair(n.cfg.HostCertPath, n.cfg.HostCertKeyPath)
 	if err != nil {
 		return err
 	}
 
+	n.client, err = newMTLSHttpClient(keypair)
+	return err
+}
+
+func newMTLSHttpClient(keypair tls.Certificate) (*http.Client, error) {
+	tlsConfig := &tls.Config{
+		Certificates:       []tls.Certificate{keypair},
+		InsecureSkipVerify: tlsInsecureSkipVerify,
+	}
+	return &http.Client{
+		Timeout: requestTimeout,
+		Transport: &http.Transport{
+			TLSClientConfig: tlsConfig,
+		},
+	}, nil
+}
+
+func prometheusRemoteWrite(httpClient *http.Client, cfg *config.Config, httpRequest *http.Request) error {
 	var attempt uint = 0
 	var maxRetryWait = time.Duration(cfg.WriteRetryMaxIntSec) * time.Second
 	retryWait := time.Duration(cfg.WriteRetryMinIntSec) * time.Second
 
 	for attempt < cfg.WriteRetryAttempts {
-		client, err := NewMTLSHttpClient(cfg)
-		if err != nil {
-			return fmt.Errorf("error in Http Client: %w", err)
-		}
-		resp, err := client.Do(req)
+		resp, err := httpClient.Do(httpRequest)
 
 		if err != nil {
 			return fmt.Errorf("PrometheusRemoteWrite: %w", err)
@@ -81,7 +113,8 @@ func PrometheusRemoteWrite(hostinfo *hostinfo.HostInfo, cfg *config.Config, samp
 	return nil
 }
 
-func NewPrometheusRequest(hostinfo *hostinfo.HostInfo, cfg *config.Config, samples []prompb.Sample) (*http.Request, error) {
+func newPrometheusRequest(hostinfo *hostinfo.HostInfo, cfg *config.Config, samples []prompb.Sample) (
+	*http.Request, error) {
 	writeRequest := hostInfo2WriteRequest(hostinfo, samples)
 	compressedData, err := writeRequest2Payload(writeRequest)
 	if err != nil {
